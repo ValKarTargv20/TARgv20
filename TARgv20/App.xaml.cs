b945d67c985d1209f21db7b28d2d34f2216d734a
opilane <opilane@HP-CZC6297BBL>
2022-03-07 12:49:51 +0200

    Add project files.

---
+++ TARgv20/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TARgv20
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent(); //dlja xml
+
+            MainPage = new MainPage(); //zamena nachalnoj stranicy
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
